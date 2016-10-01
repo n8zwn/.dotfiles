df867c055cfa8c46cb9adac7c46ebd7efa987963
Spencer Heywood <l.spencer.heywood@gmail.com>
2016-09-30 22:22:19 -0600

    Added Pushbullet functions

---
+++ scripts/auth.py
@@ -0,0 +1,9 @@
+from pushbullet import Pushbullet
+
+api_key = 'o.MzwjL0OASrMHwKazQ0lxyoNxCgYvcv4R'
+
+pb = Pushbullet(api_key)
+
+
+### Declare specific device for pb-text.py ###
+device = pb.devices[1]
