6dfff89fca8fcfbf4576436522c8d28ef994d4d4
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-02-09 10:29:21 -0700

    Update arch-deps.sh

--- scripts/arch-deps.sh
+++ scripts/arch-deps.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-pacaur -S vim xorg-server xorg-xinit python-pip compton xterm termite i3 network-manager-applet polybar pulseaudio keychain gnome-keyring 
+pacaur -S alacritty-git vim xorg-server xorg-xinit python-pip compton xterm termite i3 network-manager-applet polybar pulseaudio keychain gnome-keyring 
+
+#Install fundle
+curl -sfL https://git.io/fundle-install | fish
