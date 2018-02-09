cf3c7721dcc89924838ed155fd8d7970b5d15628
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-02-09 11:35:09 -0700

    Update arch-deps.sh

--- scripts/arch-deps.sh
+++ scripts/arch-deps.sh
@@ -2,8 +2,6 @@
 
 pacaur -S alacritty-git fundle-git fish rofi ttf-dejavu vim firefox xorg-server xorg-xinit python-pip compton xterm termite i3 network-manager-applet polybar pulseaudio keychain gnome-keyring 
 
-#Install fundle
-curl -sfL https://git.io/fundle-install | fish
 
 #Change shell to fish
 chsh -s /usr/bin/fish
