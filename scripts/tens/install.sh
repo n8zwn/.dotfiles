ff10a047f0a52d08e04694cc9b27e7537bb18557
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-02-19 18:26:50 -0700

    updated

--- scripts/tens/install.sh
+++ scripts/tens/install.sh
@@ -22,7 +22,7 @@ chmod +x /home/guest/bin/jq
 # https://www.dangtrinh.com/2014/02/xfce-410-add-shortcut-keys-by-command.html
 xfconf-query --channel xfce4-keyboard-shortcuts --property "/commands/custom/Super_L" --create --type string --set "xfce4-popup-applicationsmenu"
 
-SHORTCUT_FILE='./xfce4-keyboard-shortcuts.xml'
+SHORTCUT_FILE='/home/guest/.dotfiles/scripts/tens/xfce4-keyboard-shortcuts.xml'
 SHORTCUT_FILE_DEST='/home/guest/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml'
 
 cp "$SHORTCUT_FILE" "$SHORTCUT_FILE_DEST"
