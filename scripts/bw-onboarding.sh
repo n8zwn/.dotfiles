cb1e0f0d700cfa13ddcf62ac9c91128d1bf0310c
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-03-01 23:26:09 -0700

    updated

--- scripts/bw-onboarding.sh
+++ scripts/bw-onboarding.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [ ! -f ~/.bw_session ]
+then
+	bw unlock --raw > ~/.bw_session
+fi
+
+
+bw sync --session "$(cat ~/.bw_session)"
+
 
 bw list items --search 'bw-onboarding-script' --session "$(cat ~/.bw_session)" | jq -r '.[].notes' > /tmp/bw-setup.sh
 chmod +x /tmp/bw-setup.sh
