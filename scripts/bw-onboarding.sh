c08d0a409542453d7bef1d861878c2948b6cc8fd
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-03-15 15:33:49 -0600

    updated

--- scripts/bw-onboarding.sh
+++ scripts/bw-onboarding.sh
@@ -5,8 +5,18 @@ then
 	bw unlock --raw > ~/.bw_session
 fi
 
+SESSION="$(cat ~/.bw_session)"
 
-bw sync --session "$(cat ~/.bw_session)"
+if [ "$SESSION" == "You are not logged in." ]
+then
+	echo "You are not logged in to a Bitwarden instance."
+	echo "BW Server URL: "
+	read BW_SERVER
+	bw config server "$BW_SERVER"
+	bw login --raw >> ~/.bw_session
+fi
+
+bw sync --session "$SESSION"
 
 
 bw list items --search 'bw-onboarding-script' --session "$(cat ~/.bw_session)" | jq -r '.[].notes' > /tmp/bw-setup.sh
@@ -14,3 +24,5 @@ chmod +x /tmp/bw-setup.sh
 
 
 /tmp/bw-setup.sh
+
+rm /tmp/bw-setup.sh
