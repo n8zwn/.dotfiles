9851c065439ef10b5b735938f73934114deaf3b5
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-03-21 10:13:47 -0600

    updated

---
+++ config/fish/functions/macos.fish
@@ -0,0 +1,12 @@
+function macos
+    set working_dir (pwd)
+    set macos_dir ~/.vagrant/macos
+    mkdir -p $macos_dir
+    cd $macos_dir
+    if test ! -f $macos_dir/Vagrantfile
+        vagrant init macoslinux/macoslinux
+    end
+    vagrant up
+    vagrant ssh
+    cd $working_dir
+end
