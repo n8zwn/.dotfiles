da176f0391e9f69696d06b7b328d058fcbe162be
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-04-22 15:21:37 -0600

    updated

--- config/fish/functions/mitmf.fish
+++ config/fish/functions/mitmf.fish
@@ -1,3 +1,19 @@
 function mitmf
-    docker run online2311/mitmf $argv
+   if uname -a | grep -q linux
+       set WORKING_DIR (pwd)
+       mkdir -p ~/git/
+       cd ~/git
+       sudo apt-get install python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev libcapstone3 libcapstone-dev libffi-dev file -y
+       if lsvirtualenv | grep -q MITMf
+           workon MITMf
+       else
+           mkvirtualenv MITMf
+       end
+       git clone https://github.com/byt3bl33d3r/MITMf
+       cd MITMf
+       git submodule init
+       git submodule update --recursive
+       pip2 install -r requirements.txt
+       python2 mitmf.py --help
+   end
 end
