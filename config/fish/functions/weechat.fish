2f7d82f605cc953e0567273494059bb966f12418
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-06-28 12:40:25 -0600

    updated function

--- config/fish/functions/weechat.fish
+++ config/fish/functions/weechat.fish
@@ -1,10 +1,15 @@
 function weechat
-  if docker ps -a | grep -q weechat
+  if docker ps | grep -q weechat
     docker exec -it weechat weechat 
   else
-    if test -z ~/.weechat-docker
-      mkdir -p ~/.weechat-docker
+    if docker ps -a | grep -q weechat
+      docker start weechat
+      docker exec -it weechat weechat
+    else
+      if test -z ~/.weechat-docker
+        mkdir -p ~/.weechat-docker
+      end
+      docker run -it --name weechat -e UID=1000 -e GID=1000 -v ~/.weechat-docker:/weechat jkaberg/weechat $argv
     end
-    docker run -it --name weechat -e UID=1000 -e GID=1000 -v ~/.weechat-docker:/weechat jkaberg/weechat $argv
   end
 end
