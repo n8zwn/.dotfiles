8cf73a8c1d33dfec427df1e522124b7ffbbbda50
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-06-28 11:12:01 -0600

    update security functions

--- config/fish/functions/kali.fish
+++ config/fish/functions/kali.fish
@@ -1,14 +1,3 @@
 function kali
-    set working_dir (pwd)
-    set kali_dir ~/.vagrant/kali
-    mkdir -p $kali_dir
-    cd $kali_dir
-    if test ! -f $kali_dir/Vagrantfile
-        vagrant init offensive-security/kali-linux
-        rm $kali_dir/Vagrantfile
-        curl 'https://gist.githubusercontent.com/heywoodlh/ab3706570b1ad6da233053e27527060d/raw/3f394d22c79bb97420d193d1b27b14071633fd0b/kali-vagrantfile' -o $kali_dir/Vagrantfile
-    end
-    vagrant up
-    vagrant ssh
-    cd $working_dir
+  docker run -it --rm --net host --privileged -e DISPLAY -v $HOME/.Xauthority:/root/.Xauthority booyaabes/kali-linux-full /bin/bash
 end
