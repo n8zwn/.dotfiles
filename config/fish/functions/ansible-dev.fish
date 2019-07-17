aa0ca55f31e3cb5f70e3f23b21a0137a7874fac8
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-07-16 18:40:07 -0600

    added ansible-dev vagrant scripts

--- config/fish/functions/ansible-dev.fish
+++ config/fish/functions/ansible-dev.fish
@@ -9,6 +9,5 @@ function ansible-dev
         curl 'https://gist.githubusercontent.com/heywoodlh/ac1bf93bade2176d95a3000ccab35b8b/raw/409c8bbf085a5684774529c6e1750a76e11ca4b1/ubuntu-ansible-vagrant' -o $ubuntu_dir/Vagrantfile
     end
     vagrant up
-    vagrant ssh
     cd $working_dir
 end
