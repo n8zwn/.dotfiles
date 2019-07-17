46b605672b386cc039b28d47ea7161a3117e9642
Spencer Heywood <l.spencer.heywood@gmail.com>
2019-07-16 18:34:43 -0600

    added ansible-dev vagrant scripts

---
+++ config/fish/functions/ansible-dev.fish
@@ -0,0 +1,14 @@
+function ansible-dev
+    set working_dir (pwd)
+    set ubuntu_dir ~/.vagrant/ansible-dev
+    mkdir -p $ubuntu_dir
+    cd $ubuntu_dir
+    if test ! -f $ubuntu_dir/Vagrantfile
+        vagrant init ubuntu/bionic64
+        rm $ubuntu_dir/Vagrantfile
+        curl 'https://gist.githubusercontent.com/heywoodlh/ac1bf93bade2176d95a3000ccab35b8b/raw/409c8bbf085a5684774529c6e1750a76e11ca4b1/ubuntu-ansible-vagrant' -o $ubuntu_dir/Vagrantfile
+    end
+    vagrant up
+    vagrant ssh
+    cd $working_dir
+end
